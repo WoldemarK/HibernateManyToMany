40ce0b65e3c6f53dc7a7d8b337a00ed396a09ffd
WoldemarK <kovtynov.vladimir@gmail.com>
2022-12-01 01:33:44 +0300

    a

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,50 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            Transaction transaction = session.beginTransaction();
+
+//            Movie movie = new Movie("SomeMovie", 1994);
+//            Actor actor1 = new Actor("Bob", 22);
+//            Actor actor2 = new Actor("Tom", 33);
+//
+//            movie.setActor(new ArrayList<>(List.of(actor1, actor2)));
+//
+//            actor1.setMovies(new ArrayList<>(Collections.singleton(movie)));
+//            actor2.setMovies(new ArrayList<>(Collections.singleton(movie)));
+//
+//            session.save(movie);
+//
+//            session.save(actor1);
+//            session.save(actor2);
+
+            Movie movie = session.get(Movie.class,1);
+            System.out.println(movie.getActor());
+
+            Actor actor = session.get(Actor.class,2);
+            actor.getMovies().forEach(System.out::println);
+
+            transaction.commit();
+            session.close();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,2 +1,73 @@
-package org.example;public class Movie {
+package org.example;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Movie")
+public class Movie {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year_of_production")
+    private int yearOfProduction;
+
+    @ManyToMany(mappedBy = "movies")
+    private List<Actor> actor;
+
+    public Movie() {
+    }
+
+    public Movie(String name, int yearOfProduction) {
+        this.name = name;
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public List<Actor> getActor() {
+        return actor;
+    }
+
+    public void setActor(List<Actor> actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", yearOfProduction=" + yearOfProduction +
+                '}';
+    }
 }
